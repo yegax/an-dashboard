e9b28c9b241126592900a43d71b753bdb190b656
yegax <yegax.digital@gmail.com>
2022-04-09 21:15:48 -0400

    updt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { TodayComponent } from './modules/today/today.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TodayComponent,
   ],
   imports: [
     BrowserModule,
